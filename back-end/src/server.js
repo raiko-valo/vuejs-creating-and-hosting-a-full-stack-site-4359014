588b1f90a3786b731140b599e899d0672f33ac6b
Raiko Valo <valo.raiko@gmail.com>
2025-06-19 12:38:53 +0300

    nodemon

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,12 +1,27 @@
-import express from 'express'
+import express from 'express';
+import { products, cartItems } from './temp-data';
 
-const app = express()
+const app = express();
 
 app.get('/hello', (req, res) => {
   res.send('Hello world!')
-})
+});
 
-const port = 8000
+app.get('/products', (req, res) => {
+  res.json(products)
+});
+
+app.get('/products/:productId', (req, res) => {
+  const productId = req.params.productId
+  const product = products.find(el => el.id = productId)
+  res.json(product)
+});
+
+app.get('/cart', (req, res) => {
+  res.json(cartItems)
+});
+
+const port = 8000;
 app.listen(port, () => {
   console.log(`Server is listening on ${port}`)
-})
+});
