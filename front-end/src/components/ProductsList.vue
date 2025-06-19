f5ea3df73d73244fb106a45cc17c5ea8c738a615
Raiko Valo <valo.raiko@gmail.com>
2025-06-19 10:13:30 +0300

    Products list

---
+++ front-end/src/components/ProductsList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="grid-wrap">
+    <div v-for="product in props.products" :key="product.id" class="product-item">
+      <img :src="product.imageName"/>
+      <h3 class="product-name">{{ product.name }}</h3>
+      <p class="product-price">{{ product.price }}</p>
+      <router-link :to="`/products/${product.id}`">
+        <button>View Details</button>
+      </router-link>
+    </div>
+  </div>
+</template>
+<script setup>
+import { defineProps } from 'vue';
+
+
+
+const props = defineProps({
+  products: { type: Array, default: () => [] },
+})
+</script>
--- front-end/src/pages/ProductsPage.vue
+++ front-end/src/pages/ProductsPage.vue
@@ -1,19 +1,9 @@
 <template>
   <h1>Products</h1>
-  <div class="grid-wrap">
-    <div v-for="product in products" :key="product.key" class="product-item">
-      <img :src="product.imageName"/>
-      <h3 class="product-name">{{ product.name }}</h3>
-      <p class="product-price">{{ product.price }}</p>
-      <router-link :to="`/products/${product.id}`">
-        <button>View Details</button>
-      </router-link>
-    </div>
-  </div>
+  <ProductsList :products="products" />
 </template>
 
 <script setup>
+import ProductsList from '@/components/ProductsList.vue';
 import { products } from '../temp-data'
-
-
 </script>
