9e65ffbf7bf2044b1cb1bd3df1bf7ce98d95733f
Raiko Valo <valo.raiko@gmail.com>
2025-06-19 13:02:05 +0300

    add to cart api

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,7 +1,11 @@
 import express from 'express';
-import { products, cartItems } from './temp-data';
+import { products as productsRaw, cartItems as cartItemsRaw } from './temp-data';
 
 const app = express();
+app.use(express.json())
+
+let cartItems = cartItemsRaw
+let products = productsRaw
 
 app.get('/hello', (req, res) => {
   res.send('Hello world!')
@@ -13,13 +17,22 @@ app.get('/products', (req, res) => {
 
 app.get('/products/:productId', (req, res) => {
   const productId = req.params.productId
-  const product = products.find(el => el.id = productId)
+  const product = productsRaw.find(el => el.id = productId)
   res.json(product)
 });
 
 app.get('/cart', (req, res) => {
-  res.json(cartItems)
+  res.json(cartItemsRaw)
 });
+
+app.post('/cart', (req, res) => {
+  console.log("siin", req)
+  const productId = req.body.id
+  console.log("id", productId)
+  const product = productsRaw.find(el => el.id = productId)
+  cartItems.push(product)
+  res.json(cartItems)
+})
 
 const port = 8000;
 app.listen(port, () => {
